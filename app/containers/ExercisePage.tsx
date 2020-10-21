b611e936d814d8aaf12662bb7f1e86ccb0c99cfd
Lucas Andrade <llucasandradd@gmail.com>
2020-10-21 09:59:56 -0300

    Fix total rounds set value on ExercisePage

--- app/containers/ExercisePage.tsx
+++ app/containers/ExercisePage.tsx
@@ -6,7 +6,7 @@ import { secondsToMiliseconds } from 'app/modules/exercises/utils';
 const { remote } = require('electron');
 
 // 3 seconds: time between each repeat for exercises with duration === false
-const EXERCISE_REPEAT_TIME = 3;
+const EXERCISE_REPEAT_TIME = 1;
 
 // 1 second
 const TIMER_INTERVAL = 1000;
@@ -32,8 +32,10 @@ export const ExercisePage = () => {
       return exercise.repeat * EXERCISE_REPEAT_TIME;
   };
 
+  // set all exercise values when currentExercise change
   useEffect(() => {
     const firstDuration = exerciseDuration();
+    setTotalRounds(exercises[currentExercise].rounds);
     setTimer(firstDuration);
     setRound(1);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -41,10 +43,12 @@ export const ExercisePage = () => {
 
   const clearCurrentRoundInterval = (timerInterval: NodeJS.Timer) => {
     clearInterval(timerInterval);
+    // start next round
     if (round < totalRounds) {
       setTimer(exerciseDuration());
       setRound(round + 1);
     }
+    // start next exercise
     if (round === totalRounds) {
       setCurrentExercise(currentExercise + 1);
     }
@@ -53,7 +57,9 @@ export const ExercisePage = () => {
   useEffect(() => {
     let timerInterval: NodeJS.Timer;
     let temporaryTimer = timer;
+
     if (round > 0 && timer > 0) {
+      // time interval for each round
       timerInterval = setInterval(() => {
         temporaryTimer -= 1;
         setTimer(temporaryTimer);

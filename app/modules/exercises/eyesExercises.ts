dfe0c9fc69c21b9f05e6a601faf565a70e090ad9
Lucas Andrade <llucasandradd@gmail.com>
2020-11-21 19:47:31 -0300

    Add eyes exercises data

---
+++ app/modules/exercises/eyesExercises.ts
@@ -0,0 +1,26 @@
+export const eyesExercises = {
+  1: {
+    name: 'upDown',
+    description:
+      'Mova os olhos para cima e para baixo, igual a animação abaixo, sem pressa e durante 20 segundos.',
+    duration: 20,
+  },
+  2: {
+    name: 'leftRight',
+    description:
+      'Mova os olhos para direita e para a esquerda, igual a animação abaixo, sem pressa e durante 20 segundos.',
+    duration: 20,
+  },
+  3: {
+    name: 'clockWise',
+    description:
+      'Gire os olhos no sentido horário, igual a animação abaixo, sem pressa e durante 20 segundos.',
+    duration: 20,
+  },
+  4: {
+    name: '20-20-20-rule',
+    description:
+      'Olhe para um ponto fixo há pelo menos 20 metros longe de você, durante 20 segundos.',
+    duration: 20,
+  },
+};

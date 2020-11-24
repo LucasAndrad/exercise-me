c5ac1befd36ed90a7fe112dec9fc11d03a0cb8e0
Lucas Andrade <llucasandradd@gmail.com>
2020-11-24 10:33:21 -0300

    Finish the design for Start Day page

--- app/constants/colors.ts
+++ app/constants/colors.ts
@@ -1,3 +1,6 @@
 export const colors = {
   prussianBlue: '#102e4a',
+  buttonBlue: '#5887ff',
+  buttonHoverBlue: '#6b66ff',
+  white: '#ffffff',
 };
--- app/containers/StartDayPage.tsx
+++ app/containers/StartDayPage.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useHistory } from 'react-router-dom';
 import { setStartDay } from 'app/modules/settings/actions';
-import { routes } from 'app/constants/routes';
+import { routes, colors } from 'app/constants';
+import { Button } from 'app/components';
+
+const StartDayButton = styled(Button)`
+  padding: 15px 30px;
+  font-size: 1.8rem;
+  width: 200px;
+  text-transform: uppercase;
+  font-family: MontserratLight;
+  letter-spacing: 2px;
+`;
+
+const Container = styled.div`
+  text-align: center;
+  width: 100%;
+  height: 100vh;
+  overflow-y: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const FullRow = styled.div`
+  width: 100%;
+`;
+
+const H1 = styled.h1`
+  font-size: 3rem;
+  text-transform: uppercase;
+  font-family: MontserratLight;
+  margin-top: ${(props) => props.mt || 'auto'};
+`;
 
 export const StartDayPage = () => {
   const history = useHistory();
@@ -12,11 +44,13 @@ export const StartDayPage = () => {
   };
 
   return (
-    <div>
-      <h2>Start your day now!</h2>
-      <button type="button" onClick={() => handleStartDay()}>
-        Start
-      </button>
-    </div>
+    <Container>
+      <div>
+        <H1 mt="0px">Start your day!</H1>
+        <StartDayButton type="button" onClick={() => handleStartDay()}>
+          Start
+        </StartDayButton>
+      </div>
+    </Container>
   );
 };
